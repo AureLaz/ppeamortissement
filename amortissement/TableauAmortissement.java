94870552941903e63b11f525e35b605ad356e022
AureLaz <aurelien.lazaro@gmail.com>
2015-11-27 11:50:25 +0100

    Modification test
    TEST

--- amortissement/TableauAmortissement.java
+++ amortissement/TableauAmortissement.java
@@ -16,6 +16,11 @@ public class TableauAmortissement
 	{
 		nbLignes = credit.duree();
 		Ligne[] tableauAmort = new Ligne[nbLignes];
+		tableauAmort[0] = Ligne.premiereLigne(credit);
+		for (int i = 1; i < tableauAmort.length; i++) 
+		{
+			;
+		}
 	}
 	
 	/**
